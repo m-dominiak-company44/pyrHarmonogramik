5be8dfe7c236e65a5699657e2b67b2d02f56f340
MaciejDominiak <45820718+maciek1651g@users.noreply.github.com>
2025-05-15 18:47:39 +0200

    Fixes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/pyrharmonogramik/',
+    base: '/pyrHarmonogramik/',
     plugins: [react()],
     resolve: {
         alias: {
