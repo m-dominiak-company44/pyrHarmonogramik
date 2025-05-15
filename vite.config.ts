29906aaf3e0ce7d98f967a47957ee2223b498ce6
MaciejDominiak <45820718+maciek1651g@users.noreply.github.com>
2025-05-15 18:37:11 +0200

    Changed name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/pyrkon-44/',
+    base: '/pyrharmonogramik/',
     plugins: [react()],
     resolve: {
         alias: {
